ba77018b5a5e449b6666a787cfc6d361e82c0a07
missTinuade <estherjolugba@yahoo.com>
2020-05-12 00:52:01 +0000

    Radiobutton is checked

--- app/src/main/java/com/tinuade/droidcafe/OrderActivity.java
+++ app/src/main/java/com/tinuade/droidcafe/OrderActivity.java
@@ -3,7 +3,9 @@ package com.tinuade.droidcafe;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -23,5 +25,29 @@ public class OrderActivity extends AppCompatActivity {
     }
 
     public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked=((RadioButton)view).isChecked();
+        switch(view.getId()){
+            case R.id.sameday:
+                if (checked){
+                    // Same day service
+                    displayToast(getString(R.string.same_day_messenger_service));
+                }
+                break;
+            case R.id.nextday:
+                if (checked){
+                    // next day service
+                    displayToast(getString(R.string.next_day_ground_delivery));
+                }
+                break;
+            case R.id.pickup:
+                if (checked){
+                    //pick up
+                    displayToast(getString(R.string.pick_up));
+                }
+        }
+    }
+    public void displayToast(String message){
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
     }
 }
