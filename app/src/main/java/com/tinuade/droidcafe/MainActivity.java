c8beefdd405612a9359c46fadc1a24ebd1714071
missTinuade <estherjolugba@yahoo.com>
2020-05-15 17:29:24 +0000

    added Intent class to the menu action_order

--- app/src/main/java/com/tinuade/droidcafe/MainActivity.java
+++ app/src/main/java/com/tinuade/droidcafe/MainActivity.java
@@ -91,7 +91,9 @@ public class MainActivity extends AppCompatActivity {
                 displayToast(getString(R.string.action_favorites_message));
                 return true;
             case R.id.action_order:
-                displayToast(getString(R.string.action_order_message));
+                Intent intent=new Intent(MainActivity.this,OrderActivity.class);
+                intent.putExtra(EXTRA_MESSAGE,mOrderMessage);
+                startActivity(intent);
                 return true;
             case R.id.action_status:
                 displayToast(getString(R.string.action_status_message));
