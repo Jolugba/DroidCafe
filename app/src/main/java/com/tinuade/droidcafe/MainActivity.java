38095aec3c235e660c32a8e8f3f2b1ff7139e80b
missTinuade <estherjolugba@yahoo.com>
2020-05-15 17:15:39 +0000

    toolbar set up amd menu action done

--- app/src/main/java/com/tinuade/droidcafe/MainActivity.java
+++ app/src/main/java/com/tinuade/droidcafe/MainActivity.java
@@ -9,19 +9,23 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE =
             "com.example.android.droidcafe.extra.MESSAGE";
-   private String mOrderMessage;
+    private String mOrderMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
 
 
     }
+
 
     public void showDonutOrders(View view) {
         mOrderMessage = getString(R.string.donut_order_message);
@@ -29,14 +33,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
     public void showFroyoOrder(View view) {
-          mOrderMessage = getString(R.string.froyo_order_message);
+        mOrderMessage = getString(R.string.froyo_order_message);
         displayToast(mOrderMessage);
     }
 
     public void showIceCreamOrders(View view) {
-          mOrderMessage = getString(R.string.ice_cream_order_message);
+        mOrderMessage = getString(R.string.ice_cream_order_message);
         displayToast(mOrderMessage);
     }
 
@@ -45,8 +48,9 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(getApplicationContext(), message,
                 Toast.LENGTH_SHORT).show();
     }
+
     public void fabButtonClicked(View view) {
-        Intent intent=new Intent(this,OrderActivity.class);
+        Intent intent = new Intent(this, OrderActivity.class);
         intent.putExtra(EXTRA_MESSAGE, mOrderMessage);
         startActivity(intent);
 
@@ -62,10 +66,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main,menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
 
     }
+
     /**
      * Handles app bar item clicks.
      *
@@ -78,10 +83,23 @@ public class MainActivity extends AppCompatActivity {
         //Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id=item.getItemId();
-        if (id==R.id.action_setting){
-            return true;
-        }else
+        switch (item.getItemId()) {
+            case R.id.action_contacts:
+                displayToast(getString(R.string.action_contact_message));
+                return true;
+            case R.id.action_favourites:
+                displayToast(getString(R.string.action_favorites_message));
+                return true;
+            case R.id.action_order:
+                displayToast(getString(R.string.action_order_message));
+                return true;
+            case R.id.action_status:
+                displayToast(getString(R.string.action_status_message));
+                return true;
+            default:
+        }
+
+
         return super.onOptionsItemSelected(item);
     }
 }
